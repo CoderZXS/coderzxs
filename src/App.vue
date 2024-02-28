d8d669e85c4a69ab79c67dc97a7d76d036037821
CoderZXS <coderzxs@foxmail.com>
2024-02-28 16:16:50 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>你好.</h1>
+  <p style="font-weight: 500; font-size: 2.2em">我是CoderZXS.</p>
+  <p>
+    我是一名程序员，在深圳工作。会点前端和iOS，现在主要做前端开发。
+  </p>
+  <p style="font-weight: 500; font-size: 2.2em">
+    联系方式：
+  </p>
+  <p>
+    微信: <a>CoderZXS</a>
+  </p>
+  <p>
+    抖音: <a>CoderZXS</a>
+  </p>
+  <p>
+    手机: <a href="tel:13714151040">13714151040</a>
+  </p>
+  <p>
+    Email: <a href="mailto:coderzxs@foxmail.com">coderzxs@foxmail.com</a>
+  </p>
+</template>
+
+<style scoped></style>
